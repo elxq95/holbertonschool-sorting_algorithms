a3577e1e59dadfa9192e33faab4a5d2ae9267455
double0kung <00kung@gmail.com>
2024-08-08 08:35:03 +0000

    updated sort.h and added selection sort file

--- sort.h
+++ sort.h
@@ -1,5 +1,7 @@
-#ifndef SORT_HEADER
-#define SORT_HEADER
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -15,8 +17,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Function prototypes */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-#endif
+void selection_sort(int *array, size_t size);
+void swap_ints(int *a, int *b);
+
+#endif /* SORT_H */
