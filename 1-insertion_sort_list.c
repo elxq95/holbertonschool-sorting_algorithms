0e8ccce0b36b19c0c875b24d90e68dcee029a1c3
elxq95 <leongxiqern12@gmail.com>
2024-08-08 03:14:45 +0000

    wip

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked
+ * list of integers in ascending order using the insertion
+ * sort algorithm
+ * @list: pointer to a pointer
+ * Return: nothing
+ */
+
+
